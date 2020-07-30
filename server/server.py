e099bfae524a915446049b0d48984e36d4c70495
Pradyumna Das <pd10@illinois.edu>
2020-07-30 20:55:04 +0530

    added the porter stemmer code by Vivake Gupta. header on porter.py has more details

--- server/server.py
+++ server/server.py
@@ -1,8 +1,7 @@
-# This is the AWS lambda code. It was written directly in the online lambda editor
-
 import json
 import re
 import boto3
+from thesaurus import get_related_words
 
 comprehend = boto3.client(service_name='comprehend', region_name='ap-southeast-1')
 
@@ -23,14 +22,12 @@ def get_sentiment_score_aws(sentence_list):
             score = score + sentiment_info["Negative"] * -1
         elif result["Sentiment"] == "NEUTRAL":
             score = score + sentiment_info["Neutral"] * 0.3
-        else:
-            score = score + 0
-            #ignore the cases where the sentiment is mixed
-        
+    
+    score = round(score / len(text_sent_list) if len(sentence_list) > 0 else 0, 4)
     return (text_sent_list, score)
 
 def get_keyword_score_response(text_sent_list, score):
-    resp_obj = {"Score": score}
+    resp_obj = {"score": score}
     resp_list = []
     
     for text, sentiment in text_sent_list:
@@ -39,26 +36,33 @@ def get_keyword_score_response(text_sent_list, score):
     resp_obj["sent_class"] = resp_list
     
     return resp_obj
+    
+def get_related_sentences(sentences, rel_words):
+    result = []
+    
+    for s in sentences:
+        words_in_sent = set(s.split(" "))
+        if len(words_in_sent.intersection(rel_words)) > 0:
+            result.append(s)
+                
+    return result
 
 def lambda_handler(event, context):
     sentences = [p.lower().strip() for p in re.split(r"[.?!;\n]+", event["text"])]
-    k1_sents = []
-    k2_sents = []
+    keyword1 = event["k1"].strip()
+    keyword2 = event["k2"].strip()
     
-    for s in sentences:
-        if event["k1"] in s:
-            k1_sents.append(s)
-        elif event["k2"] in s:
-            k2_sents.append(s)
+    k1_rel_words = get_related_words(keyword1)
+    k2_rel_words = get_related_words(keyword2)
+    
+    k1_sents = get_related_sentences(sentences, k1_rel_words)
+    k2_sents = get_related_sentences(sentences, k2_rel_words)
     
     text_sent_list_k1, overall_score_k1 = get_sentiment_score_aws(k1_sents)
     text_sent_list_k2, overall_score_k2 = get_sentiment_score_aws(k2_sents)
     
     result = {}
-    result[event["k1"]] = get_keyword_score_response(text_sent_list_k1, overall_score_k1)
-    result[event["k2"]] = get_keyword_score_response(text_sent_list_k2, overall_score_k2)
+    result[keyword1] = get_keyword_score_response(text_sent_list_k1, overall_score_k1)
+    result[keyword2] = get_keyword_score_response(text_sent_list_k2, overall_score_k2)
     
-    return {
-        'statusCode': 200,
-        'body': json.dumps(result)
-    }
+    return result
--- server/thesaurus.py
+++ server/thesaurus.py
@@ -1,20 +1,40 @@
 import urllib3
 import json
+from porter import PorterStemmer
 
 def get_related_words(root_word):
-    rel_words = {root_word}
-    api_url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{}?key=api_key"
-
-    http_client = urllib3.PoolManager()
-
-    response = http_client.request("GET", api_url.format(root_word))
-
-    data = json.loads(response.data.decode('utf-8'))[0]
+    rel_words = set()
     
-    rel_words = rel_words.union(set(data["meta"]['stems']))
-    rel_words = rel_words.union(set(data["meta"]['syns'][0][:5]))
-    rel_words = rel_words.union(set(data["meta"]['ants'][0][:5]))
+    if len(root_word) > 0:
+        rel_words.add(root_word)
+        api_url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{}?key=4307e0db-8af2-43bc-8ab1-977f32898c4e"
+    
+        http_client = urllib3.PoolManager()
+    
+        response = http_client.request("GET", api_url.format(root_word))
+    
+        data = json.loads(response.data.decode('utf-8'))[0]
+        
+        rel_words = rel_words.union(set(data["meta"]['stems']))
+        
+        if len(data["meta"]['syns']) > 0:
+            synonyms = set(data["meta"]['syns'][0][:5])
+            synonyms_stemmed = get_stemmed_words(synonyms)
+            rel_words = rel_words.union(synonyms).union(synonyms_stemmed)
+        if len(data["meta"]['ants']) > 0:
+            antonyms = set(data["meta"]['ants'][0][:5])
+            antonyms_stemmed = get_stemmed_words(antonyms)
+            rel_words = rel_words.union(antonyms).union(antonyms_stemmed)
+            
+    print(f"Root word: {root_word}, Related words: {rel_words}")
 
     return rel_words
 
-print(get_related_words("hygiene"))
+def get_stemmed_words(word_list):
+    stemmer = PorterStemmer()
+    stemmed_words = set()
+    
+    for word in word_list:
+        stemmed_words = stemmed_words.union(stemmer.stem(word, 0,len(word)-1))
+        
+    return stemmed_words
